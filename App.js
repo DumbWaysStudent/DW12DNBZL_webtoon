70aede2b93c1c2e3625b6d809abf90ed29e929cb
retuyu88 <andikaherup@gmail.com>
2019-10-10 14:29:08 +0700

    done share feature on Detail screen

--- App.js
+++ App.js
@@ -9,9 +9,29 @@ import Detail_episodes from './screens/Detail_episodes'
 import My_favourite_screen from './screens/My_favourite_screen'
 import Profile from './screens/Profile'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import {Share} from 'react-native'
 import { Container,Header,Text, Body, Content, Form, Item, Input, Button,Toast,Root, Label,InputGroup, Footer, FooterTab, CardItem,Card, Left, Right, ListItem} from 'native-base'
 
+onShare = async () => {
+  try {
+    const result = await Share.share({
+      message:
+        'React Native | A framework for building native apps using React',
+    });
 
+    if (result.action === Share.sharedAction) {
+      if (result.activityType) {
+        // shared with activity type of result.activityType
+      } else {
+        // shared
+      }
+    } else if (result.action === Share.dismissedAction) {
+      // dismissed
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+};
 const switchContainer = createSwitchNavigator({
   Login : Login_screen,
   'Fyscreen': {
@@ -48,29 +68,10 @@ const switchContainer = createSwitchNavigator({
 })
 
 const AppContainer = createAppContainer(switchContainer)
-onShare = async () => {
-  try {
-    const result = await Share.share({
-      message:
-        'React Native | A framework for building native apps using React',
-    });
 
-    if (result.action === Share.sharedAction) {
-      if (result.activityType) {
-        // shared with activity type of result.activityType
-      } else {
-        // shared
-      }
-    } else if (result.action === Share.dismissedAction) {
-      // dismissed
-    }
-  } catch (error) {
-    alert(error.message);
-  }
-};
 
 export default class App extends Component{
-
+  
   render(){
     return <AppContainer/>
   }
--- screens/Detail_screen.js
+++ screens/Detail_screen.js
@@ -69,26 +69,7 @@ export default class Detail_screen extends Component{
       </ListItem>
     );
   }
-  onShare = async () => {
-    try {
-      const result = await Share.share({
-        message:
-          'React Native | A framework for building native apps using React',
-      });
-
-      if (result.action === Share.sharedAction) {
-        if (result.activityType) {
-          // shared with activity type of result.activityType
-        } else {
-          // shared
-        }
-      } else if (result.action === Share.dismissedAction) {
-        // dismissed
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-  };
+  
   render() {
     
     return (
